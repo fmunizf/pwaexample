5086fccebb62c7f2ce702bc8a292ef17843cdc02
fmunizf <felipemuniz101@gmail.com>
2025-01-27 09:13:29 -0500

    following week 2 code pdf up to page 4

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { doc, getDocs, addDoc, updateDoc, getFirestore, collection } from
+"firebase/firestore";
+const firebaseConfig = {
+apiKey: "your-api-key",
+authDomain: "your-auth-domain",
+projectId: "your-project-id",
+storageBucket: "your-storage-bucket",
+messagingSenderId: "your-messaging-sender-id",
+appId: "your-app-id"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//
+
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
@@ -17,3 +33,14 @@ taskList.addEventListener('click', (e) => {
         e.target.remove();
     }
 });
+//Service worker registration
+const sw = new URL('service-worker.js', import.meta.url)
+if ('serviceWorker' in navigator) {
+const s = navigator.serviceWorker;
+s.register(sw.href, {
+scope: '/pwaexample/'
+})
+.then(_ => console.log('Service Worker Registered for scope:', sw.href,
+'with', import.meta.url))
+.catch(err => console.error('Service Worker Error:', err));
+}
